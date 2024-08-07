98d69eab066b9498a9ae6f7bd2d878af17c9aa0c
AromalP2002 <akhilaromal938@gmail.com>
2024-08-07 15:33:53 +0530

    new

---
+++ operators.py
@@ -0,0 +1,61 @@
+# print(10+4)
+# print('aro'+'b')
+# print(10-3)
+# print(10/5)
+# print(10*2)
+# print('python'*3)
+# print(10%5)
+# print(5//2)
+
+
+# a=10
+# print(a)
+# a=10
+# # a=a+5
+# a+=5
+# print(a)
+# a=10
+# a-=5
+# print(a)
+# a=10
+# a*=5
+# print(a)
+# a=10
+# a%=5
+
+# print(a)
+# a=10
+# # a**5
+# a**=5
+# print(a)
+
+# print(10==10)
+# print(10!=9)
+# print(10!=10)
+# print(10>9)
+# print(10>11)
+# print(10<9)
+# print(10<11)
+# print(10>=10)
+# print(10>=11)
+# print(10<=10)
+# print(10<=9)
+# print(10<=11)
+
+
+# print(10==10 and 10>=5)
+# print(10==10 and 10>=11)
+# print(10>=11 and 10>5)
+# print(10<9 and 10>11)
+# print(10==10 or 10>9)
+# print(1==1 or 11<9)
+# print(10<9 or 10>11)
+# print(not(10==10))
+# print(not(10>9))
+# print(not(10>11))
+
+
+l={1,2,3,10}
+print(10 in l)
+k=[1,2,3,10]
+print(10 not in k)
