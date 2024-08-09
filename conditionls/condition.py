5e3b29c4c48d3bf28fd358d9e335e3aa18acd20f
AromalP2002 <akhilaromal938@gmail.com>
2024-08-09 15:58:54 +0530

    hallo

--- conditionls/condition.py
+++ conditionls/condition.py
@@ -1,9 +1,32 @@
-a=input("enter a number:")
-b=input("enter a number:")
-if a>b:
-    print("number 1 is greater")
-else:
-    print("number 1 is not greater")
+# a=input("enter a number:")
+# b=input("enter a number:")
+# if a>b:
+#     print("number 1 is greater")
+# else:
+#     print("number 1 is not greater")
 
-    
+a=int(input("1st no"))
+b=int(input("2nd no"))
+if a>b:
+    print("gtr")
+elif a==b:
+    print('equl')  
+else:
+    print('less')
+
+a=int(input("1st no"))
+b=int(input("2nd no"))
+c=int(input("3rd no"))
+if a>b:
+    if(a>c):
+        print("a is gtr")
+    else:
+        print("c is gtr")
+else:
+    if(b>c):
+        print("b is gtr")
+    else:
+        print("c is gtr")
+            
+  
 
