4c5e586f39271dea35e23f3a53e41fa1a4c7647c
AromalP2002 <akhilaromal938@gmail.com>
2024-08-12 04:59:03 +0100

    updated

--- conditionls/condition.py
+++ conditionls/condition.py
@@ -5,28 +5,74 @@
 # else:
 #     print("number 1 is not greater")
 
-a=int(input("1st no"))
-b=int(input("2nd no"))
-if a>b:
-    print("gtr")
-elif a==b:
-    print('equl')  
-else:
-    print('less')
+# a=int(input("1st no"))
+# b=int(input("2nd no"))
+# if a>b:
+#     print("gtr")
+# elif a==b:
+#     print('equl')  
+# else:
+#     print('less')
 
-a=int(input("1st no"))
-b=int(input("2nd no"))
-c=int(input("3rd no"))
-if a>b:
-    if(a>c):
-        print("a is gtr")
-    else:
-        print("c is gtr")
-else:
-    if(b>c):
-        print("b is gtr")
-    else:
-        print("c is gtr")
+# a=int(input("1st no"))
+# b=int(input("2nd no"))
+# c=int(input("3rd no"))
+# if a>b:
+#     if(a>c):
+#         print("a is gtr")
+#     else:
+#         print("c is gtr")
+# else:
+#     if(b>c):
+#         print("b is gtr")
+#     else:
+#         print("c is gtr")
             
   
+# slary=int(input("enter the slary"))
+# servi=int(input("enter the servi"))
+# if servi>5:
+#     a=slary*5/100
+#     b=a+slary
+#     print("slary is",b)
+#     print("bonus is",a)
+# else:
+#     print("slary is", slary)   
+#     print("no bonus") 
+
+
+
+# n1=int(input("enter the number of unit"))
+
+
+# if n1<100:
+#     print("no charge")
+# elif n1>100 and n1<200:
+#     unit=n1-100
+#     np=unit*5
+#     print("charge is",np) 
+# else:
+#     unit1=n1-200
+#     npp=unit1*10+500
+#     print("charge is",npp)
+        
+
+num=int(input("enter a number :"))
+if num ==1:
+    print("sunday")
+elif num ==2:
+    print("monday")
+elif num==3:
+    print("tuesday")
+elif num ==3:
+    print("wensday")
+elif num==4:
+    print("thuesday")
+elif num==5:
+    print("friday")
+elif num==6:
+    print("saturday")
+else:
+                                
+
 
