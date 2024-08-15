cfb18ee790f25537060084c3f0d03c6f9d492c82
AromalP2002 <akhilaromal938@gmail.com>
2024-08-15 04:59:20 +0100

    updated

--- loop/while.py
+++ loop/while.py
@@ -17,5 +17,35 @@
 #     a+=1
 # print(s)    
 
+# a=int(input("enter the 1st number"))
+# b=int(input("enter the 2nd number"))
 
+# while a<=b:
+#     if (a%2) !=0:
+#         print(a)
+#     a+=1 
+   
+
+#  a=int(input("enter the 1st number"))
+# b=int(input("enter the 2nd number"))
+    
+# a=int(input("enter the 1st number"))
+# b=int(input("enter the 2nd number"))
+# sum=0
+# even=0
+# odd=0
+# while a<=b:
+#     sum+=a
+#     if a%2==0:
+#         even+=a
+#     else:
+#         odd+=a
+#     a+=1
+# print(sum)
+# print(even)
+# print(odd)
+
+a=int(input("enter a number"))
+i=1
+while a<=1:
     
