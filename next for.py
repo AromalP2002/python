05a9cd7aaff682b6902754a33988c3d5f36509fe
AromalP2002 <akhilaromal938@gmail.com>
2024-08-20 04:37:10 +0100

    updated

---
+++ next for.py
@@ -0,0 +1,38 @@
+# pattern design
+
+# for i in range(3):
+#     for j in range(3):
+#         print(j,end="  ")
+#     print()
+
+    
+# print('welcome',end=" ")
+# print('to',end="  ")
+# print('all',end="  ")
+
+
+# a=0
+# for i in range(3):
+#     for j in range(3):
+#         print(a,end="  ")
+#         a+=1
+#     print()    
+
+
+# for i in range(3):
+#     for j in range(3):
+#       print(i+j,end=" ")
+#     print() 
+
+
+for i in range(4):
+    for j in range(3):
+        if i%2 ==0:
+           print(j,end=" ")
+        else:
+            print(2-j,end=" ")
+    print()        
+       
+
+
+    print()    
