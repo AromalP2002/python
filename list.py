ddef471f5a0a09d50c7e534c15ff64680dbb394a
AromalP2002 <akhilaromal938@gmail.com>
2024-08-24 04:43:27 +0100

    perfect

--- list.py
+++ list.py
@@ -45,10 +45,50 @@
 # print(l)
 #index method
 
-l=[10,20,30,8,5]
-print(l.index(20))
-print(l.count(10))
-l.sort()
-print(l)
-l.reverse()
-print(l)
+# l=[10,20,30,8,5]
+# print(l.index(20))
+# print(l.count(10))
+# l.sort()
+# print(l)
+# l.reverse()
+# print(l)
+
+
+# l=[1,10,12,'abc',8.5]
+# sum=0
+# for i in l:
+#     if type(i)==int or type (i)==float:
+#         sum+=i
+# print(sum) 
+# 
+#        
+#    sum of odd & even  
+# l=[10,1,2,3,5,8,6]
+# o=0
+# e=0
+# for i in l:
+#     if i%2==0:
+#         o+=i
+#     else:
+#         e+=i
+# print('sum of odd no.s=',o)
+# print('sum of even no.s=',e)            
+
+l=[10,1,2,3,5,8,6,1,2,8]
+# for i in l:
+#     if l.count(i)>=2:
+#         l.remove(i)
+# print(l)        
+
+# s=set(l)
+# l=list(s)
+# print(l)
+
+# in,not in
+
+# l1=[]
+# for i in l:
+#     if i not in l1:
+#         l1.append(i)
+# print(l1) 
+#        
