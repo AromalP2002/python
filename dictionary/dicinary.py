d0fbf5b8530847ac81022c93306be67e55957eb3
AromalP2002 <akhilaromal938@gmail.com>
2024-09-06 09:22:25 +0530

    welcome

--- dictionary/dicinary.py
+++ dictionary/dicinary.py
@@ -38,8 +38,20 @@ data=[{'name':'a','age':20},
 # print(a)
 # print(b)
 
-n={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
-a=int(input("enter a number"))
-while a<0:
-    b=a%10
-    rev=rev*10+b
+# n={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
+# a=int(input("enter a number: "))
+# s=''
+# while a>0:
+#     d=a%10
+#     s=n[d]+' '+s
+#     a//=10
+# print(s)
+
+# adding 
+
+l=[{'name':'a','age':20,'project':['ems','sms']}]
+# print(l[0]['project'][0])
+new_pro='cms'
+l[0]['project'].append(new_pro)
+print(l)
+
