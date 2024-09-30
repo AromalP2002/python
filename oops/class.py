4329a4813fdc9e53590d0b8ae9f9efabd0c50c50
AromalP2002 <akhilaromal938@gmail.com>
2024-09-30 09:03:51 +0530

    perfect

---
+++ oops/class.py
@@ -0,0 +1,41 @@
+# class syn:
+#     def python():
+#         print('python prg')
+#     def java():
+#         print('java prg')
+#     def php():
+#             print('php prg')
+# manu=syn
+# manu.python()
+# anu=syn
+# anu.java()
+
+
+class Bank:
+    def __init__(s):
+        s.name=input('name')
+        s.age=(input('age'))
+        s.bal=0
+
+    def deposit(self,amt):
+        self.bal+=amt
+        print('deposit')
+    def withdraw(self,amt):
+        print('withdraw')
+        self.bal-=amt
+        print('withdrow')
+    def balance(self):
+        print('balance',self.bal)
+
+obj=Bank()
+obj.deposit(5000)
+obj.balance()
+obj.withdraw(2000)
+obj.balance()
+print(obj.bal)
+
+obj1=Bank()
+obj1.deposit(10000)
+obj1.balance()
+
+
