85045c3dabceb6aca7cbbfc2447b842d05505021
AromalP2002 <akhilaromal938@gmail.com>
2024-10-01 09:05:31 +0530

    sucess

--- oops/class.py
+++ oops/class.py
@@ -11,31 +11,31 @@
 # anu.java()
 
 
-class Bank:
-    def __init__(s):
-        s.name=input('name')
-        s.age=(input('age'))
-        s.bal=0
+# class Bank:
+#     def __init__(s):
+#         s.name=input('name')
+#         s.age=(input('age'))
+#         s.bal=0
 
-    def deposit(self,amt):
-        self.bal+=amt
-        print('deposit')
-    def withdraw(self,amt):
-        print('withdraw')
-        self.bal-=amt
-        print('withdrow')
-    def balance(self):
-        print('balance',self.bal)
+#     def deposit(self,amt):
+#         self.bal+=amt
+#         print('deposit')
+#     def withdraw(self,amt):
+#         print('withdraw')
+#         self.bal-=amt
+#         print('withdrow')
+#     def balance(self):
+#         print('balance',self.bal)
 
-obj=Bank()
-obj.deposit(5000)
-obj.balance()
-obj.withdraw(2000)
-obj.balance()
-print(obj.bal)
+# obj=Bank()
+# obj.deposit(5000)
+# obj.balance()
+# obj.withdraw(2000)
+# obj.balance()
+# print(obj.bal)
 
-obj1=Bank()
-obj1.deposit(10000)
-obj1.balance()
+# obj1=Bank()
+# obj1.deposit(10000)
+# obj1.balance()
 
 
---
+++ oops/in heritance.py
@@ -0,0 +1,84 @@
+# class syn:
+
+#     def python(self):
+#         self .a=10
+#         print('python')
+#     def php(self):
+#         print('php')
+
+# class novavi(syn):
+#     def DM(self):
+#         print('dm works')
+#     def web_dev(self):
+#         print('user dev')
+
+
+# novavi_staff=novavi()
+# novavi_staff.DM()
+# novavi_staff.python()
+# print(novavi_staff.a)
+# std1=syn()
+# std1.python()
+
+#multiple inheritance
+
+# class father:
+#     def shop (self):
+#         print('shop')
+#     def car(self):
+#         print('car')
+# class mother:
+#     def dress_shop(self):
+#         print('dress_shop')
+
+# class child(father,mother):
+#     def bike(self):
+#         print('bike')
+# sanu=child()
+# sanu.shop()
+# sanu.car(),sanu.dress_shop()
+
+# class school:
+#     def principal(self):
+#         print('principal')
+#     def teachers(self):
+#         print('teachers')
+
+# class tution :
+#     def notes(self):
+#         print('notes')
+#     def PTA(self):
+#         print('PTA_Meeting')
+# class student (school,tution):
+#     def books(self):
+#         print('books')
+#     def library(self):
+#         print('library')
+
+# anu=student()
+# anu.principal()
+# anu.teachers()
+# anu.notes()
+
+#multilevel inhertiance
+
+
+# class c_u:
+#     def exam(self):
+#         print('exam')
+#     def result(self):
+#         print('result')
+# class clg(c_u):
+#     def project(self):
+#         print('project')
+# class std(clg):
+#     def uniform(self):
+#         print('uniform')
+
+# sanu=std()
+# sanu .exam()
+# sanu.result()
+# sanu.project()
+# sanu.uniform()
+
+                
