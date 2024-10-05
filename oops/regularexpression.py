5abe5e9b1c915c647f72428466e4c7de9019f6e1
AromalP2002 <akhilaromal938@gmail.com>
2024-10-05 09:10:09 +0530

    perfect ok

---
+++ oops/regularexpression.py
@@ -0,0 +1,35 @@
+
+import re
+# a='abcd'
+# print(re.search('[a-z].4',a))
+# print(re.search('[a-z].5',a))
+
+
+# a='abcd789'
+# print(re.search('[a-z].*[0-9]',a))
+
+# 
+
+
+# a='978654321'
+# if len(a)==10 and re.search('[6-9].{9}',a) and a.isdigit():
+#     print('valid')
+# else:
+#     print('invalid') 
+
+
+
+# a="samP541e@gmail.com"
+# if re.search('^[a-z].*@gmail.com$',a):
+#     print('valid')
+# else:
+#     print('invalid') 
+
+
+# password validation
+
+# a='ABCDH'
+# if re.search('^[A-z0-9].*[@#&$0-9]',a) and not(a.isdigit()) and len(a)>=8:
+#     print('valid')
+# else:
+#     print('not')
