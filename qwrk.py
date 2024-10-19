77ad2fa5bd3f4268c345185cb1c1abaa8b83291e
AromalP2002.com <akhilaromal938@gmail.com>
2024-10-19 09:11:28 +0530

    ntho cheyth

---
+++ qwrk.py
@@ -0,0 +1,28 @@
+# for i in range(1,4):
+#     for j in range(1,4):
+#         if j%2==0:
+#             print('2',end=" ")
+#         else:
+#             print('1',end=' ') 
+#     print('')       
+
+
+
+# x=4
+# for i in  range(1,4):
+#     a=1
+#     for j in range(1,x):
+#         print(a,end=' ')
+#         a+=1
+#     x-=1
+#     print(' ')
+
+
+X=4
+for i in range(1,4):
+    a=65
+    for j in range(1,X):
+        print(chr(a),end=' ')
+        a+=1
+    X-=1
+    print()    
